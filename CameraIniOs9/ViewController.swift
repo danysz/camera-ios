801de7d448c869c1fd3907cb5717682af551c853
Daniel Szasz <danysz@gmail.com>
2016-01-08 00:14:30 +0200

    Initial Commit

---
+++ CameraIniOs9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraIniOs9
+//
+//  Created by Daniel Szasz on 08/01/2016.
+//  Copyright © 2016 Daniel Szasz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
